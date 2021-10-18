d22b13b11e796555674c5074fe48ca275e5a2adc
JINs1106 <ideale95@naver.com>
2021-10-18 10:50:55 +0900

    notice

--- src/main/java/edu/spring/project/FinalprojectApplication.java
+++ src/main/java/edu/spring/project/FinalprojectApplication.java
@@ -1,20 +1,26 @@
 package edu.spring.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import chatbot.ChatbotController;
+import notice.NoticeController;
+import notice.NoticeDAO;
 
 @SpringBootApplication
 
 @ComponentScan
-@ComponentScan(basePackageClasses = ChatbotController.class)
+//@ComponentScan(basePackageClasses = ChatbotController.class)
+@ComponentScan(basePackageClasses = NoticeController.class)
+@MapperScan
+@MapperScan(basePackageClasses = NoticeDAO.class)
 public class FinalprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalprojectApplication.class, args);
-		System.out.println("ai 9003서버 시작");
+		System.out.println("ai 9009서버 시작");
 		
 	}
 
